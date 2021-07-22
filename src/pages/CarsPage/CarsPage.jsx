7a15b4eb33a4b73a8ddbfe748a188c9321c06606
nirch <nirh2@yahoo.com>
2021-07-22 12:41:19 +0300

    cars page implementation

--- src/pages/CarsPage/CarsPage.jsx
+++ src/pages/CarsPage/CarsPage.jsx
@@ -3,12 +3,45 @@ import CarModel from '../../model/CarModel';
 
 function CarsPage() {
     const [cars, setCars] = useState([
-        new CarModel
-    ])
+        new CarModel("22-322-22", "Toyota", "Yaris", 2002, 250000),
+        new CarModel("21-322-22", "Toyota", "Corola", 2015, 115000),
+        new CarModel("24-322-22", "Hyundai", "i30", 2010, 180000),
+    ]);
 
+    // finding the car with the highest km per year
+    let highestCar = cars[0];
+    for (const car of cars) {
+        if (car.kmPerYear() > highestCar.kmPerYear()) {
+            highestCar = car;
+        }
+    }
+
+    const carTableRows = cars.map(car =>
+        // <CarTableRow car={car} isHighestCar={car === highestCar}/>
+        <p>{car.brand + car.model}</p>
+    );
+
+    function addCar() {
+        const newCar = new CarModel("23-291-22", "Subaru", "Forster", 2018, 100000);
+        setCars(cars.concat(newCar));
+    }
     return (
         <div>
-            Cars Page
+            <table className="table">
+                <thead>
+                    <tr>
+                        <th>Brand</th>
+                        <th>Model</th>
+                        <th>Year</th>
+                        <th>KM</th>
+                        <th>KM per Year</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    {carTableRows}
+                </tbody>
+            </table>
+            <button className="btn btn-primary" onClick={addCar}>Add Car</button>
         </div>
     );
 }
